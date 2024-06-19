3c778b92c60a47e3bb3ccaa4cf80378db65448d1
Luis Fernando Patiño Londoño <luizlondono@gmail.com>
2024-06-19 04:19:41 -0500

    Sustentación versión 1

--- Models/Sustentacion.py
+++ Models/Sustentacion.py
@@ -3,10 +3,53 @@ import pandas as pd
 import Matrix_PD_version as mpd
 
 def subsystem(dict, subsystem):
-    subsystem = subsystem.split('/')
-    print(subsystem[0])
-    print(subsystem[1])
+    column_indices = [i for i, c in enumerate(subsystem.split('/')[0]) if c == '0']
+    row_indices = [i for i, c in enumerate(subsystem.split('/')[1]) if c == '0']
 
+    new_dict2 = dict
+    new_dict2 = mpd.marginalize_column(new_dict2, 3)
+
+    for index in sorted(column_indices, reverse=True):
+        dict = mpd.marginalize_column(dict, index)
+    
+    new_dict = dict
+    keys = list(dict.keys())
+
+    
+    for index in sorted(row_indices, reverse=True):
+        aux_dict = {}
+        aux_dict2 = {}
+        for key in keys:
+            if key[index] == '0':
+                aux_dict[key[:index] + key[index + 1:]] = new_dict[key]
+                aux_dict2[key[:index] + key[index + 1:]] = new_dict2[key]
+        keys = list(aux_dict.keys())
+        new_dict = aux_dict
+        new_dict2 = aux_dict2
+
+    PD_dict = pd.DataFrame(new_dict).transpose()
+    print(PD_dict)
+
+    column_indices = sum(1 for caracter in subsystem.split('/')[0] if caracter != '0')
+
+    dicts = getIndividualMatrixes(new_dict2, 3)
+
+    print("\n")
+
+    for dict in dicts:
+        pd_dict = pd.DataFrame(dict).transpose()
+
+    return new_dict, dicts
+
+def getIndividualMatrixes(dict, column_indices):
+    dicts = []
+    for index in range(column_indices):
+        aux_dict = dict
+        for index2 in range(column_indices, 0, -1):
+            if (index2 - 1) != index:
+                aux_dict = mpd.marginalize_column(aux_dict, index2 - 1)
+        dicts.append(aux_dict)
+    return dicts
 
 ABCD = {
     '0000': {'0000': 0.2401, '1000': 0.1029, '0100': 0.1029, '1100': 0.0441, '0010': 0.1029, '1010': 0.0441, '0110': 0.0441, '1110': 0.0189, '0001': 0.1029, '1001': 0.0441, '0101': 0.0441, '1101': 0.0189, '0011': 0.0441, '1011': 0.0189, '0111': 0.0189, '1111': 0.0081},
@@ -31,4 +74,12 @@ PD_ABCD = pd.DataFrame(ABCD).transpose()
 print(PD_ABCD)
 print("\n\n\n")
 
-subsystem(ABCD, 'ABC/ABC')
+# key completa = 1000
+new_dict, dicts = subsystem(ABCD, 'ABC0/ABC0')
+mpd.bottom_up(dicts[0], dicts[1], dicts[2], key = '100')
+
+# new_dict, dicts = subsystem(ABCD, 'A0C0/AB00')
+# mpd.bottom_up(dicts[0], dicts[1], dicts[2], key = '10')
+
+# new_dict, dicts = subsystem(ABCD, 'A0C0/ABC0')
+# mpd.bottom_up(dicts[0], dicts[1], dicts[2], key = '100')
